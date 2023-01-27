09dd8755976fa58b0176833a91a17d66724dd18c
pitjohann <johann.maillard@edu.devinci.fr>
2023-01-27 19:02:41 +0100

    Front more or less good, now trying to link with back

---
+++ myapp/src/routes/login/+page.svelte
@@ -0,0 +1,36 @@
+<div class="auth-page">
+	<div class="container page">
+		<div class="row">
+			<div class="col-md-6 offset-md-3 col-xs-12">
+				<h1 class="text-xs-center">Login</h1>
+				<p class="text-xs-center">
+					<a href="/signUp">Need an account?</a>
+				</p>
+
+
+
+				<form  method="POST">
+					<fieldset class="form-group">
+						<input
+								class="form-control form-control-lg"
+								name="username"
+								type="text"
+								required
+								placeholder="Your Name"
+						/>
+					</fieldset>
+					<fieldset class="form-group">
+						<input
+								class="form-control form-control-lg"
+								name="password"
+								type="password"
+								required
+								placeholder="Password"
+						/>
+					</fieldset>
+					<button class="btn btn-lg btn-primary pull-xs-right" type="submit">Login</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ myapp/src/routes/signUp/+page.svelte
@@ -0,0 +1,42 @@
+<svelte:head>
+	<title>Sign Up</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="auth-page">
+	<div class="container page">
+		<div class="row">
+			<div class="col-md-6 offset-md-3 col-xs-12">
+				<h1 class="text-xs-center">Sign up</h1>
+				<p class="text-xs-center">
+					<a href="/login">Have an account?</a>
+				</p>
+
+
+
+				<form method="POST">
+					<fieldset class="form-group">
+						<input
+								class="form-control form-control-lg"
+								name="username"
+								type="text"
+								required
+								placeholder="Your Name"
+						/>
+					</fieldset>
+
+					<fieldset class="form-group">
+						<input
+								class="form-control form-control-lg"
+								name="password"
+								type="password"
+								required
+								placeholder="Password"
+						/>
+					</fieldset>
+					<button class="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
