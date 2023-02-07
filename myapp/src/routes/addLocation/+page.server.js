a82b10f3f5815e890dc47244109271c572edd0f2
pitjohann <johann.maillard@edu.devinci.fr>
2023-02-07 17:29:15 +0100

    AddLocation is working, just the back crashes if the information given isn't in the good format.

--- myapp/src/routes/addLocation/+page.server.js
+++ myapp/src/routes/addLocation/+page.server.js
@@ -2,15 +2,19 @@ import { fail, redirect } from '@sveltejs/kit';
 import * as api from '$lib/api.js';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ parent }) {
-    const { user } = await parent();
-    if (user) throw redirect(307, '/');
+export async function load({cookies }) {
+    if (!cookies){
+        return fail(401);
+    }
+    let token = cookies.get('jwt')
+
 }
 
 /** @type {import('./$types').Actions} */
 export const actions = {
     default: async ({ cookies, request }) => {
         const data = await request.formData();
+        const token = cookies.get("jwt")
 
         const location = {
             filmType: data.get('filmType'),
@@ -25,7 +29,7 @@ export const actions = {
             year: data.get('year')
         };
 
-        const body = await api.post('locations/',  location );
+        const body = await api.post('locations/',  location,token);
 
         if (body.errors) {
             return fail(401, body);
