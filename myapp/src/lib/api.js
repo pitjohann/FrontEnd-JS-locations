cd6c89b9b418cda6a8ad0ae99a2a97c24bc075bc
pitjohann <johann.maillard@edu.devinci.fr>
2023-02-16 09:16:06 +0100

    testing vercel

--- myapp/src/lib/api.js
+++ myapp/src/lib/api.js
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 
-const base = 'http://localhost:3000';
+const base = 'https://web-backend-loc.onrender.com';
 
 async function send({ method, path, data, token }) {
     const opts = { method, headers: {} };
